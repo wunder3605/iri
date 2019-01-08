256d8827b320e5bd002df4015001eb750975749b
Gal Rogozinski <gal.rogozinski@iota.org>
2019-01-08 17:39:04 +0200

    rocksdb initialization

--- src/test/java/com/iota/iri/TransactionValidatorTest.java
+++ src/test/java/com/iota/iri/TransactionValidatorTest.java
@@ -36,7 +36,7 @@ public class TransactionValidatorTest {
     tangle = new Tangle();
     tangle.addPersistenceProvider(
         new RocksDBPersistenceProvider(
-            dbFolder.getRoot().getAbsolutePath(), logFolder.getRoot().getAbsolutePath(),1000));
+            dbFolder.getRoot().getAbsolutePath(), logFolder.getRoot().getAbsolutePath(),1000, Iota.COLUMN_FAMILIES, Iota.METADATA_COLUMN_FAMILY));
     tangle.init();
     TipsViewModel tipsViewModel = new TipsViewModel();
     MessageQ messageQ = Mockito.mock(MessageQ.class);
