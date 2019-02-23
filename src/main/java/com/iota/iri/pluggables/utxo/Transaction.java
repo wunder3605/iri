ad372059fa9c330c2128f46c5d61c0944d82f8f2
wangjunqing <wunder3605@126.com>
2019-02-23 20:16:41 +0800

    [fix #151] Restore UTXO when restarting
    Conflicts:
    	src/main/java/com/iota/iri/pluggables/utxo/Transaction.java
    	src/main/java/com/iota/iri/pluggables/utxo/TransactionData.java
    	src/main/java/com/iota/iri/pluggables/utxo/TransactionIn.java

--- src/main/java/com/iota/iri/pluggables/utxo/Transaction.java
+++ src/main/java/com/iota/iri/pluggables/utxo/Transaction.java
@@ -2,15 +2,13 @@ package com.iota.iri.pluggables.utxo;
 
 import java.util.*;
 
-import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iota.iri.model.*;
 import com.iota.iri.utils.Converter;
 
 public class Transaction {
 
-    @JSONField(serialize=false)
-    Hash txnHash;
+    String txnHash;
 
     List<TransactionIn> inputs;
 
@@ -24,7 +22,15 @@ public class Transaction {
         return outputs;
     }
 
+<<<<<<< HEAD:src/main/java/com/iota/iri/pluggables/utxo/Transaction.java
     public int getTryteStringLen(Transaction txn) {
+=======
+    public String getTxnHash() {
+        return txnHash;
+    }
+
+    public int getTryteStringLen(Txn txn) {
+>>>>>>> fb56a6e... [fix #151] Restore UTXO when restarting:src/main/java/com/iota/iri/pluggables/utxo/Txn.java
         ObjectMapper mapper = new ObjectMapper();
         try {
             String str = mapper.writeValueAsString(txn);
