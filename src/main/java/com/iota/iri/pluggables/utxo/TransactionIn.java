ba7afecbf2770327a93a282db97c70210b24773a
wangjunqing <wunder3605@126.com>
2019-02-23 20:16:41 +0800

    [fix #151] Restore UTXO when restarting
    Conflicts:
    	src/main/java/com/iota/iri/pluggables/utxo/Transaction.java
    	src/main/java/com/iota/iri/pluggables/utxo/TransactionData.java
    	src/main/java/com/iota/iri/pluggables/utxo/TransactionIn.java

--- src/main/java/com/iota/iri/pluggables/utxo/TransactionIn.java
+++ src/main/java/com/iota/iri/pluggables/utxo/TransactionIn.java
@@ -2,12 +2,17 @@ package com.iota.iri.pluggables.utxo;
 
 import com.iota.iri.model.*;
 
+<<<<<<< HEAD:src/main/java/com/iota/iri/pluggables/utxo/TransactionIn.java
 public class TransactionIn {
     Hash txnHash;
+=======
+public class TxnIn {
+    String txnHash;
+>>>>>>> fb56a6e... [fix #151] Restore UTXO when restarting:src/main/java/com/iota/iri/pluggables/utxo/TxnIn.java
     int idx;
     String userAccount;
 
-    public Hash getTxnHash() {
+    public String getTxnHash() {
         return txnHash;
     }
 
