8fcf2e33f20abafd65bb3080382221366cf2a1d3
wangyh2016 <850138226@qq.com>
2019-03-06 13:07:26 +0800

    [fix #189] modify  docker profiles

--- scripts/tee/entrypoint.sh
+++ scripts/tee/entrypoint.sh
@@ -2,4 +2,5 @@
 #go get github.com/awalterschulze/gographviz
 #go get github.com/kylelemons/go-gypsy/yaml
 #go get github.com/wunder3605/pagerank
+python /go/src/app/modify_yaml_file.py modify_go_yaml
 sleep 100000
