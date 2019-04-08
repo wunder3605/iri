5aa0b4dda16ccced4dec6e258ea51bb4b7395545
lhfbc <lhf_bc@163.com>
2019-04-08 11:14:01 +0800

    codacy: \' to \".

--- scripts/front_end/web/src/main.js
+++ scripts/front_end/web/src/main.js
@@ -1,18 +1,18 @@
-import babelpolyfill from 'babel-polyfill'
-import Vue from 'vue'
-import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-import routes from './routes'
-import axios from 'axios'
-import Vueaxios from 'vue-axios'
+import babelpolyfill from "babel-polyfill"
+import Vue from "vue"
+import App from "./App"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-default/index.css"
+import VueRouter from "vue-router"
+import store from "./vuex/store"
+import Vuex from "vuex"
+import routes from "./routes"
+import axios from "axios"
+import Vueaxios from "vue-axios"
 import Echarts from "echarts";
 
 
-import 'font-awesome/css/font-awesome.min.css'
+import "font-awesome/css/font-awesome.min.css"
 
 Vue.use(ElementUI);
 Vue.use(VueRouter);
@@ -28,4 +28,4 @@ new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
