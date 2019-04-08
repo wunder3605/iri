1f8af6cfaf94cc08f9dab9be2beecdb8fe63e10e
wangyh2016 <850138226@qq.com>
2019-04-08 10:18:37 +0800

    Update server_deploy_batch.py

--- scripts/iota_deploy/server_deploy_batch.py
+++ scripts/iota_deploy/server_deploy_batch.py
@@ -18,18 +18,18 @@ def deploy_iri_server():
     ip_total = get_ip_list()
     ip_pub = list(ip_total.values())
     for ip_address in ip_pub:
-        oret = Popen(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker ps |grep iota-node |wc -l "], shell=False, stdout=PIPE, stderr=STDOUT)
+        oret = Popen(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker ps |grep iota-node |wc -l "], shell=False, stdout=PIPE, stderr=STDOUT)
         num_exist = oret.stdout.readlines()[1].strip()
         if int(num_exist):
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
-            call(["pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker stop iota-node"])
-            call(["pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker rm iota-node"])
-            call(["pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run  -d -p 14700:14700 -p 13700:13700 --name iota-node -v /home/trust/iri/scripts/examples/data:/iri/data -v /home/trust/iri/scripts/examples/conf/neighbors iota-node:v0.1-streamnet  /entrypoint.sh"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker stop iota-node"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker rm iota-node"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run  -d -p 14700:14700 -p 13700:13700 --name iota-node -v /home/trust/iri/scripts/examples/data:/iri/data -v /home/trust/iri/scripts/examples/conf/neighbors iota-node:v0.1-streamnet  /entrypoint.sh"])
         else:
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
-            call(["pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker run  -d -p 14700:14700 -p 13700:13700 --name iota-node -v /home/trust/iri/scripts/examples/data:/iri/data -v /home/trust/iri/scripts/examples/conf/neighbors iota-node:v0.1-streamnet  /entrypoint.sh"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@"+ip_address, "-x", "\"-oStrictHostKeyChecking=no\"", "sudo docker run  -d -p 14700:14700 -p 13700:13700 --name iota-node -v /home/trust/iri/scripts/examples/data:/iri/data -v /home/trust/iri/scripts/examples/conf/neighbors iota-node:v0.1-streamnet  /entrypoint.sh"])
     return 'success'
 
 #cli deploy
@@ -40,18 +40,18 @@ def deploy_cli_server():
         oret = Popen(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker ps |grep iota-cli |wc -l "], shell=False, stdout=PIPE,stderr=STDOUT)
         num_exist = oret.stdout.readlines()[1].strip()
         if int(num_exist):
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker stop iota-cli"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker rm iota-cli"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run -d -p 5000:5000 --name iota-cli iota-cli:v0.1-streamnet /docker-entrypoint.sh"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker stop iota-cli"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker rm iota-cli"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run -d -p 5000:5000 --name iota-cli iota-cli:v0.1-streamnet /docker-entrypoint.sh"])
         else:
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
-            call(["pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run -d -p 5000:5000 --name iota-cli iota-cli:v0.1-streamnet /docker-entrypoint.sh"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo rm -rf ~/iri/scripts/examples/data/testnetdb.log"])
+            call(["/usr/local/bin/pssh", "-i", "-H", "trust@" + ip_address, "-x", "\"-oStrictHostKeyChecking=no\"","sudo docker run -d -p 5000:5000 --name iota-cli iota-cli:v0.1-streamnet /docker-entrypoint.sh"])
     return 'success'
 
-# add neighbors
+# add and remove neighbors
 def link_iri_server(input_data):
     with open("topology.txt", 'r') as list_file:
         ip_list = list_file.read()
@@ -59,10 +59,11 @@ def link_iri_server(input_data):
         result = re.findall(moudle, ip_list)
         add_ip = [x for x in result[::2]]
         desc_ip = [x for x in result[1::2]]
-        for i in range(0,len(add_ip)):
+        num = len(add_ip)
+        for i in range(0,num):
             k = add_ip[i].replace('\'','')
             v = desc_ip[i].replace('\'','')
-            Popen(['python', 'add_neighbors_batch.py', input_data, k, v])
+            Popen(['/usr/bin/python', 'add_neighbors_batch.py', input_data, k, v])
         return 'success'
 
 
